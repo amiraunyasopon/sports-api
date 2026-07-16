f5931f9e5ddafe51bb34156bd94aa3dd47cf6fb6
amiraunyasopon <amiraunyasopon@gmail.com>
2026-07-15 20:29:49 -0700

    implemented crud repositories that allow pagination

---
+++ src/main/java/repositories/PlayerRepository.java
@@ -0,0 +1,11 @@
+package repositories;
+
+import com.amir.sports.domain.entities.PlayerEntity;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PlayerRepository extends CrudRepository<PlayerEntity, Long>
+        , PagingAndSortingRepository<PlayerEntity, Long> {
+}
---
+++ src/main/java/repositories/TeamRepository.java
@@ -0,0 +1,11 @@
+package repositories;
+
+import com.amir.sports.domain.entities.TeamEntity;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TeamRepository extends CrudRepository<TeamEntity, Long>
+        , PagingAndSortingRepository<TeamEntity, Long> {
+}
