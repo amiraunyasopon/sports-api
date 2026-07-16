f6083d6b37f586e746c30716e31659c23bccc874
amiraunyasopon <amiraunyasopon@gmail.com>
2026-07-15 20:03:17 -0700

    initial commit

---
+++ src/main/java/com/amir/sports/SportsApplication.java
@@ -0,0 +1,13 @@
+package com.amir.sports;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amir/sports/SportsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amir.sports;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
