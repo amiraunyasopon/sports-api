160f891c5566b9ab5f226c5472ff3f87caf8da1e
amiraunyasopon <amiraunyasopon@gmail.com>
2026-07-19 21:08:21 -0700

    implemented create endpoint for team object

--- src/main/java/com/amir/sports/controllers/TeamController.java
+++ src/main/java/com/amir/sports/controllers/TeamController.java
@@ -1,7 +1,33 @@
 package com.amir.sports.controllers;
 
+import com.amir.sports.domain.dto.TeamDto;
+import com.amir.sports.domain.entities.TeamEntity;
+import com.amir.sports.mappers.Mapper;
+import com.amir.sports.services.TeamService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TeamController {
+
+    private TeamService teamService;
+
+    private Mapper<TeamEntity, TeamDto> teamMapper;
+
+    public TeamController(TeamService teamService, Mapper<TeamEntity, TeamDto> teamMapper)
+    {
+        this.teamService = teamService;
+        this.teamMapper = teamMapper;
+    }
+
+    @PostMapping(path = "/teams")
+    public ResponseEntity<TeamDto> createTeam(@RequestBody TeamDto team)
+    {
+        TeamEntity teamEntity = teamMapper.mapFrom(team);
+        TeamEntity savedTeamEntity = teamService.save(teamEntity);
+        return new ResponseEntity<>(teamMapper.mapTo(savedTeamEntity), HttpStatus.CREATED);
+    }
 }
--- src/test/java/com/amir/sports/controllers/TeamControllerIntegrationTests.java
+++ src/test/java/com/amir/sports/controllers/TeamControllerIntegrationTests.java
@@ -1,4 +1,52 @@
 package com.amir.sports.controllers;
 
+import com.amir.sports.TestDataUtil;
+import com.amir.sports.domain.entities.TeamEntity;
+import com.amir.sports.services.TeamService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import tools.jackson.databind.ObjectMapper;
+
+@SpringBootTest
+@ExtendWith(SpringExtension.class)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+@AutoConfigureMockMvc
 public class TeamControllerIntegrationTests {
+    
+    private TeamService teamService;
+    
+    private MockMvc mockMvc;
+    
+    private ObjectMapper objectMapper;
+    
+    @Autowired
+    public TeamControllerIntegrationTests(MockMvc mockMvc, TeamService teamService)
+    {
+        this.mockMvc = mockMvc;
+        this.teamService = teamService;
+        this.objectMapper = new ObjectMapper();
+    }
+    
+    @Test
+    public void testThatCreateTeamSuccessfullyReturnsHttp201Created() throws Exception{
+        TeamEntity testTeamA = TestDataUtil.createTestTeamEntityA();
+        testTeamA.setId(null);
+        String authorJson = objectMapper.writeValueAsString(testTeamA);
+        mockMvc.perform(
+                MockMvcRequestBuilders.post("/teams")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(authorJson)
+        ).andExpect(
+                MockMvcResultMatchers.status().isCreated()
+        );
+    }
 }
